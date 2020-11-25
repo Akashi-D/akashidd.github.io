593cf28517cafdae2e4cbe332985091e4d15f98c
Akashi-D <64048845+Akashi-D@users.noreply.github.com>
2020-11-25 15:35:41 +0100

    My First

---
+++ The Okwuzi's.com/js/style.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $("#but11").click(function(){
+        $("#nav222").toggle();
+    });
+});
+
+$(document).ready(function(){
+   $("#button").click(function(){
+      $("#p1_contain").show();
+       $("#p1_contain2").show();
+       $("#p1_contain3").show();
+       $("#p1_contain4").show();
+       $("#p1_contain5").show();
+       $("#p1_contain6").show();
+       $("#p1_contain7").show();
+       $("#p1_contain8").show();
+   }); 
+});
